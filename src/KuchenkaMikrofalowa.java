4f29a175d8456a6b1964a46e30d8dc8b64e109f6
MaciQ1 <maciekuminski1@gmail.com>
2023-06-27 22:53:57 +0200

    Poprawienie błędów

--- src/KuchenkaMikrofalowa.java
+++ src/KuchenkaMikrofalowa.java
@@ -1,11 +1,6 @@
 class KuchenkaMikrofalowa {
     public void gotujPotrawe(Potrawa potrawa) {
         potrawa.gotuj();
-        if (potrawa instanceof PotrawaRozmrazanie) {
-            PotrawaRozmrazanie potrawaRozmrazanie = (PotrawaRozmrazanie) potrawa;
-            if (!potrawaRozmrazanie.nadajeSieDoRozmrazania()) {
-                System.out.println("Potrawa nie nadaje się do rozmrożenia");
-            }
-        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
@@ -7,9 +8,48 @@ class Main {
         BazaDanych bazaDanych = new BazaDanych("BazaDoProjektuZPO.txt");
         List<Potrawa> potrawy = bazaDanych.wczytajPotrawyZPliku();
 
-        for (Potrawa potrawa : potrawy) {
-            kuchenka.gotujPotrawe(potrawa);
-            System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        int wybor;
+        boolean wyborPoprawny = false;
+
+        System.out.println("Dostępne potrawy:");
+        for (int i = 0; i < potrawy.size(); i++) {
+            System.out.println((i + 1) + ". " + potrawy.get(i).getNazwa());
         }
+
+        while (!wyborPoprawny) {
+            System.out.print("Wybierz numer potrawy: ");
+            if (scanner.hasNextInt()) {
+                wybor = scanner.nextInt();
+                if (wybor >= 1 && wybor <= potrawy.size()) {
+                    Potrawa wybranaPotrawa = potrawy.get(wybor - 1);
+                    wyborPoprawny = true;
+
+                    System.out.println("Wybrano potrawę: " + wybranaPotrawa.getNazwa());
+
+                    System.out.print("Podaj temperaturę: ");
+                    int temperatura = scanner.nextInt();
+
+                    System.out.print("Podaj czas: ");
+                    int czas = scanner.nextInt();
+
+                    if (temperatura < wybranaPotrawa.getTemperaturaMin() || temperatura > wybranaPotrawa.getTemperaturaMax()
+                            || czas < wybranaPotrawa.getCzasMin() || czas > wybranaPotrawa.getCzasMax()) {
+                        System.out.println("Potrawa spaliła się");
+                    } else {
+                        System.out.println("Potrawa gotowa");
+                    }
+
+                    kuchenka.gotujPotrawe(wybranaPotrawa);
+                } else {
+                    System.out.println("Nieprawidłowy numer potrawy");
+                }
+            } else {
+                System.out.println("Nieprawidłowe dane wejściowe");
+                scanner.next(); // Pomija nieprawidłowe dane wejściowe
+            }
+        }
+
+        scanner.close();
     }
 }
