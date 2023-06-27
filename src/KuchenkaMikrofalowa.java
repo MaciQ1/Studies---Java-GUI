5f45913258ad2525e163f7a0d363313752381156
MaciQ1 <maciekuminski1@gmail.com>
2023-06-27 20:34:40 +0200

    Poprawienie klasy KuchenkaMikrofalowa

--- src/KuchenkaMikrofalowa.java
+++ src/KuchenkaMikrofalowa.java
@@ -2,5 +2,11 @@
 class KuchenkaMikrofalowa {
     public void gotujPotrawe(Potrawa potrawa) {
         potrawa.gotuj();
+        if (potrawa instanceof PotrawaRozmrazanie) {
+            PotrawaRozmrazanie potrawaRozmrazanie = (PotrawaRozmrazanie) potrawa;
+            if (!potrawaRozmrazanie.nadajeSieDoRozmrozenia()) {
+                System.out.println("Potrawa nie nadaje się do rozmrożenia");
+            }
+        }
     }
 }
