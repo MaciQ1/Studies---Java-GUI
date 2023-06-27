ed933e5ac59487dc1ca7c82a6def4c5a79a3c821
MaciQ1 <maciekuminski1@gmail.com>
2023-06-27 20:54:58 +0200

    Dodanie menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.List;
+import java.util.Scanner;
 
-// Klasa główna programu
 public class Main {
     public static void main(String[] args) {
         KuchenkaMikrofalowa kuchenka = new KuchenkaMikrofalowa();
@@ -8,9 +8,49 @@ public class Main {
         BazaDanych bazaDanych = new BazaDanych("BazaDoProjektuZPO.txt");
         List<Potrawa> potrawy = bazaDanych.wczytajPotrawyZPliku();
 
-        for (Potrawa potrawa : potrawy) {
-            kuchenka.gotujPotrawe(potrawa);
-            System.out.println();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("-------- MENU --------");
+        System.out.println("Wybierz potrawę do gotowania:");
+
+        for (int i = 0; i < potrawy.size(); i++) {
+            Potrawa potrawa = potrawy.get(i);
+            System.out.println((i + 1) + ". " + potrawa.getNazwa());
         }
+
+        System.out.println("0. Wyjście");
+        System.out.println("----------------------");
+        System.out.print("Wybór: ");
+
+        int wybor = scanner.nextInt();
+        scanner.nextLine(); // Pobierz znak nowej linii
+
+        while (wybor != 0) {
+            if (wybor >= 1 && wybor <= potrawy.size()) {
+                Potrawa wybranaPotrawa = potrawy.get(wybor - 1);
+                kuchenka.gotujPotrawe(wybranaPotrawa);
+                System.out.println();
+            } else {
+                System.out.println("Nieprawidłowy wybór. Spróbuj ponownie.");
+            }
+
+            System.out.println("-------- MENU --------");
+            System.out.println("Wybierz potrawę do gotowania:");
+
+            for (int i = 0; i < potrawy.size(); i++) {
+                Potrawa potrawa = potrawy.get(i);
+                System.out.println((i + 1) + ". " + potrawa.getNazwa());
+            }
+
+            System.out.println("0. Wyjście");
+            System.out.println("----------------------");
+            System.out.print("Wybór: ");
+
+            wybor = scanner.nextInt();
+            scanner.nextLine(); // Pobierz znak nowej linii
+        }
+
+        System.out.println("Zamykanie programu...");
+        scanner.close();
     }
 }
